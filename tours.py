603540ff6817e5f17a985e81be104ac426e3ea9f
difonjohaiv <edison0824@outlook.com>
2023-11-27 11:44:29 +0800

    0

--- tours.py
+++ tours.py
@@ -22,12 +22,16 @@ from prepare_graph import get_graph
 def train(model: Model, x, edge_index, edge_weight):
     model.train()
     optimizer.zero_grad()
-    edge_index_1 = dropout_adj(edge_index, edge_weight, p=drop_edge_rate_1)[0]
-    edge_index_2 = dropout_adj(edge_index, edge_weight, p=drop_edge_rate_2)[0]
+    edge_index_1, edge_attr_1 = dropout_adj(edge_index,
+                                            edge_weight,
+                                            p=drop_edge_rate_1)[0]
+    edge_index_2, edge_attr_2 = dropout_adj(edge_index,
+                                            edge_weight,
+                                            p=drop_edge_rate_2)[0]
     x_1 = drop_feature(x, drop_feature_rate_1)
     x_2 = drop_feature(x, drop_feature_rate_2)
-    z1 = model(x_1, edge_index_1, edge_weight)
-    z2 = model(x_2, edge_index_2, edge_weight)
+    z1 = model(x_1, edge_index_1, edge_attr_1)
+    z2 = model(x_2, edge_index_2, edge_attr_2)
 
     loss = model.loss(z1, z2, batch_size=0)
     loss.backward()
