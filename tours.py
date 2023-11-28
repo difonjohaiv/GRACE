bf2d9da875a0b01cd8e843ddf1e39b3a86a37f0a
snailshaw <snailshaw@126.com>
2023-11-28 11:15:40 +0800

    0

--- tours.py
+++ tours.py
@@ -84,8 +84,6 @@ if __name__ == '__main__':
     data = get_graph()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(device)
-    print(type(device))
     if str(device) == "cuda":
         print("using cuda")
     else:
