fc0a21a8b2ce2d704c330d0b9592316df344dabd
difonjohaiv <edison0824@outlook.com>
2023-12-01 16:06:10 +0800

    0

--- load_data.py
+++ load_data.py
@@ -6,6 +6,7 @@ import torch as th
 import pandas as pd
 import numpy as np
 
+
 def get_train_test(target_fn):
     train_lst = list()
     test_lst = list()
@@ -23,21 +24,22 @@ def get_train_test(target_fn):
 
 class PrepareData:
 
-    def __init__(self, args):
+    def __init__(self, graph_path: str = "data/graph", dataset: str = "R8"):
+        super(PrepareData, self).__init__()
         print("prepare data")
-        self.graph_path = "data/graph"
-        self.args = args
+        self.graph_path = graph_path
+        self.dataset = dataset
 
         # graph 利用边列表文件，构建带权重的图
         graph = nx.read_weighted_edgelist(
-            f"{self.graph_path}/{args.dataset}.txt", nodetype=int)
+            f"{self.graph_path}/{self.dataset}.txt", nodetype=int)
         print_graph_detail(graph)
         # 将networkx中的图或边列表转换为scipy稀疏矩阵格式
-        adj = nx.to_scipy_sparse_matrix(graph,
-                                        nodelist=list(
-                                            range(graph.number_of_nodes())),
-                                        weight='weight',  # 表示为加权图
-                                        dtype=np.float)
+        adj = nx.to_scipy_sparse_matrix(
+            graph,
+            nodelist=list(range(graph.number_of_nodes())),
+            weight='weight',  # 表示为加权图
+            dtype=float)
         # 矩阵对应元素相乘，multiply（条件）。不对称化邻接矩阵，利于GCN的训练
         adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
         self.adj = preprocess_adj(adj, is_sparse=True)  # 邻接矩阵归一化
@@ -45,7 +47,7 @@ class PrepareData:
         # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
         # features
-        self.nfeat_dim = graph.number_of_nodes()  # 
+        self.nfeat_dim = graph.number_of_nodes()  #
         row = list(range(self.nfeat_dim))
         col = list(range(self.nfeat_dim))
         value = [1.] * self.nfeat_dim
@@ -59,8 +61,9 @@ class PrepareData:
         # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         # target 数据真实标签
 
-        target_fn = f"data/text_dataset/{self.args.dataset}.txt"  # 标签文件名
-        target = np.array(pd.read_csv(target_fn, sep="\t", header=None)[2])  # pd中的一列
+        target_fn = f"data/text_dataset/{self.dataset}.txt"  # 标签文件名
+        target = np.array(pd.read_csv(target_fn, sep="\t",
+                                      header=None)[2])  # pd中的一列
         target2id = {label: indx for indx, label in enumerate(set(target))}
         self.target = [target2id[label] for label in target]
         self.nclass = len(target2id)
